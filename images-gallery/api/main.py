8870a50f3e5439ec04c6112b8f34da4d64258e8e
Kevin Ayers <kevin.ayers123@icloud.com>
2024-01-17 14:36:23 -0800

    enabled cors and tested api endpoint

--- images-gallery/api/main.py
+++ images-gallery/api/main.py
@@ -2,12 +2,14 @@ import os
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 
-load_dotenv(dotenv_path="./env.local")
+load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL='https://api.unsplash.com/photos/random'
 UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+DEBUG=bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file")
@@ -15,26 +17,31 @@ if not UNSPLASH_KEY:
 
 
 app = Flask(__name__)
+CORS(app)
+
+app.config["DEBUG"] = DEBUG
 
 
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-
     headers = {
 
         "Accept-Version": "v1",
-        "Authorization": "Client-ID" + UNSPLASH_KEY
-
+        "Authorization": "Client-ID " + UNSPLASH_KEY
     }
 
     params = {
         "query": word
     }
 
-    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    print(headers)
+    print(params)
 
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
+
+    print(data)
 
     return data
 
--- images-gallery/frontend/src/App.js
+++ images-gallery/frontend/src/App.js
@@ -6,18 +6,16 @@ import ImageCard from "./components/ImageCard";
 import Welcome from "./components/Welcome";
 import { Container, Row, Col } from "react-bootstrap";
 
-const UNSPASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5050";
 
-function App() {
+const App = () => {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
 
-    fetch(
-      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPASH_KEY}`
-    )
+    fetch(`${API_URL}/new-image?query=${word}`)
       .then((res) => res.json())
       .then((data) => {
         setImages([{ ...data, title: word }, ...images]);
@@ -52,6 +50,6 @@ function App() {
       </Container>
     </div>
   );
-}
+};
 
 export default App;
